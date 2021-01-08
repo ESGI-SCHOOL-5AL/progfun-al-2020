0c1cc26795a6ad8e627f815fe8f7b647ffe18063
LAURENT Louis <louis_laurent@aol.fr>
2021-01-08 15:21:16 +0100

    well ... a missing else

--- src/main/scala/projetal2020/Main.scala
+++ src/main/scala/projetal2020/Main.scala
@@ -6,25 +6,27 @@ object Main extends App {
   if (args.length == 0) {
     println("File path required")
     sys.exit(1)
-  }
+  } else {
+    val fileContents = ConfigurationParser.configurationFileToString(args(0))
+    val split = ConfigurationParser.splitByBlock(fileContents)
+    val config = ConfigurationParser.parse(split)
 
-  val fileContents = ConfigurationParser.configurationFileToString(args(0))
-  val split = ConfigurationParser.splitByBlock(fileContents)
-  val config = ConfigurationParser.parse(split)
+    val grid =
+      new Grid(config.sizeGrid(0), config.sizeGrid(0), config.lawnmowers)
+    grid.print()
 
-  val grid = new Grid(config.sizeGrid(0), config.sizeGrid(0), config.lawnmowers)
-  grid.print()
-
-  val lawnmowers = config.lawnmowers.zipWithIndex.map {
-    case (lawnmower, index) => {
-      config.instructions(index).foldLeft[Lawnmower](lawnmower) { (a, m) =>
-        a.move(m)
+    val lawnmowers = config.lawnmowers.zipWithIndex.map {
+      case (lawnmower, index) => {
+        config.instructions(index).foldLeft[Lawnmower](lawnmower) { (a, m) =>
+          a.move(m)
+        }
       }
-    }
-  }.toList
+    }.toList
 
-  val gridResult = new Grid(config.sizeGrid(0), config.sizeGrid(0), lawnmowers)
-  gridResult.print()
+    val gridResult =
+      new Grid(config.sizeGrid(0), config.sizeGrid(0), lawnmowers)
+    gridResult.print()
 
-  println(Json.stringify(JSONGrid.toJSON(config, lawnmowers)))
+    println(Json.stringify(JSONGrid.toJSON(config, lawnmowers)))
+  }
 }
